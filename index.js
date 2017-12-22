e5585efae4ad8ee09c273b23c28004c496215d7c
Michael <mbocharov@lohika.com>
2017-12-22 15:34:21 +0200

    resolve pending tests issue
    in case suit contains pending tests that disabled by keyword "xit" - mocha-finalize-each failed because "fn" property undefined for test in suit

--- index.js
+++ index.js
@@ -6,18 +6,20 @@ module.exports = function finalizeEach(suite, wrapper) {
   })
   
   suite.on('test', test => {
-    var originalFn = test.fn;
+    if(test.fn){
+      var originalFn = test.fn;
     
-    if(originalFn.length > 0) {
-      test.fn = function(done) {
-        return wrapper(Promise.fromCallback(done => {
-          return originalFn.call(this, done);
-        })).asCallback(done);
-      };
-    } else {
-      test.fn = function() {
-        return wrapper(Promise.try(() => originalFn.call(this)));
-      };
+      if(originalFn.length > 0) {
+        test.fn = function(done) {
+          return wrapper(Promise.fromCallback(done => {
+            return originalFn.call(this, done);
+          })).asCallback(done);
+        };
+      } else {
+        test.fn = function() {
+          return wrapper(Promise.try(() => originalFn.call(this)));
+        };
+      }
     }
   });
 };
