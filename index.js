60d7d4661102a004ef00da79716fbd12db63e371
Michael <mbocharov@lohika.com>
2017-12-28 17:13:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,25 +2,25 @@ var Promise = require('bluebird');
 
 module.exports = function finalizeEach(suite, wrapper) {
   suite.on('suite', childSuite => {
-      finalizeEach(childSuite, wrapper);
+    finalizeEach(childSuite, wrapper);
   })
-
+  
   suite.on('test', test => {
-      if(!test.fn) {
-          return;
-      }
-      var originalFn = test.fn;
-
-      if (originalFn.length > 0) {
-          test.fn = function (done) {
-              return wrapper(Promise.fromCallback(done => {
-                  return originalFn.call(this, done);
-              })).asCallback(done);
-          };
-      } else {
-          test.fn = function () {
-              return wrapper(Promise.try(() => originalFn.call(this)));
-          };
-      }
+    if(!test.fn){
+      return;
+    }
+    var originalFn = test.fn;
+    
+    if(originalFn.length > 0) {
+      test.fn = function(done) {
+        return wrapper(Promise.fromCallback(done => {
+          return originalFn.call(this, done);
+        })).asCallback(done);
+      };
+    } else {
+      test.fn = function() {
+        return wrapper(Promise.try(() => originalFn.call(this)));
+      };
+    }
   });
 };
